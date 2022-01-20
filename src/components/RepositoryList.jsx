e90949d5dd3ce54a1c4816b0d39b8485b046cc22
Lucas Leonardo <llbs@cin.ufpe.br>
2022-01-20 20:51:53 -0300

    Use useEffect hook to fetch data from API

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,12 +1,25 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
+
+import "../styles/repositories.scss";
 
 const repository = {
     name: "github-explorer",
     description: "Web app in React to consume Github API",
     link: "https://github.com/lucasleonardobs/github-explorer.git",
-}
+};
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.github.com/users/lucasleonardobs/repos")
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, [])
+
+    consoe.log(repositories)
+
     return (
         <section className="repository-list">
             <h1>Repository List</h1>
@@ -16,4 +29,4 @@ export function RepositoryList() {
             </ul>
         </section>
     )
-}
+};
