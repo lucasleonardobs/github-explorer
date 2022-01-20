c5fd486ab1ed28de9e00faad467ff1065eaee8f2
Lucas Leonardo <llbs@cin.ufpe.br>
2022-01-20 19:22:56 -0300

    Create a initial repository list component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,18 @@
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Repository List</h1>
+
+            <ul>
+                <li>
+                    <strong>Repo name</strong>
+                    <p>Repo description</p>
+
+                    <a href="">
+                        Access to the repository
+                    </a>
+                </li>
+            </ul>
+        </section>
+    )
+}
