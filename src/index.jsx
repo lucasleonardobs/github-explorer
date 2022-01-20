5ec7049ad6331b97d011658d4f4b92cde28e1e80
Lucas Leonardo <llbs@cin.ufpe.br>
2022-01-20 20:51:01 -0300

    Fix global style use

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,4 @@
 import { render } from 'react-dom';
 import { App } from './App';
-import { RepositoryList } from './components/RepositoryList';
 
-render(<RepositoryList />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
