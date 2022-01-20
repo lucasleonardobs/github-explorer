cf3af66d87e868bc67674d44eb10b1a3589a9db4
Lucas Leonardo <llbs@cin.ufpe.br>
2022-01-20 16:29:49 -0300

    Structuring React

--- webpack.config.js
+++ webpack.config.js
@@ -14,13 +14,7 @@ module.exports = {
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: { 
-                    presets: [
-                        '@babel/preset-env',
-                        '@babel/preset-react'
-                    ] 
-                },
+                use: 'babel-loader',
             }
         ]
     }
